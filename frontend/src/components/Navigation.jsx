eab76f5b388ff3c5ee9b957db57e615d7e32848f
LexanderO <alexanderowens1993@gmail.com>
2024-05-14 23:23:01 +0100

    removed unwanted top bar

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -10,35 +10,39 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="bg-gray-800 p-4">
-      {isAuthenticated && (
-        <div className="flex justify-between items-center">
-          <div className="text-white space-x-4">
-            <Link
-              to="/blogs"
-              className="hover:text-gray-300 transition duration-150 ease-in-out"
-            >
-              My Blogs
-            </Link>
-            <span>|</span>
-            <Link
-              to="/stored-blogs"
-              className="hover:text-gray-300 transition duration-150 ease-in-out"
-            >
-              Public Blogs
-            </Link>
-            <span>|</span>
-            <Link
-              to="/"
-              onClick={handleLogout}
-              className="hover:text-gray-300 transition duration-150 ease-in-out"
-            >
-              Logout
-            </Link>
+    <>
+      {isAuthenticated ? (
+        <nav className="bg-secondary p-4">
+          <div className="flex justify-between items-center">
+            <div className="text-white space-x-4">
+              <Link
+                to="/blogs"
+                className="hover:text-primary transition duration-150 ease-in-out"
+              >
+                My Blogs
+              </Link>
+              <span>|</span>
+              <Link
+                to="/stored-blogs"
+                className="hover:text-primary transition duration-150 ease-in-out"
+              >
+                Public Blogs
+              </Link>
+              <span>|</span>
+              <Link
+                to="/"
+                onClick={handleLogout}
+                className="hover:text-primary transition duration-150 ease-in-out"
+              >
+                Logout
+              </Link>
+            </div>
           </div>
-        </div>
+        </nav>
+      ) : (
+        ""
       )}
-    </nav>
+    </>
   );
 };
 
