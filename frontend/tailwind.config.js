a1a5db8252a7cb662f6c276b63549adc7d663d4a
LexanderO <alexanderowens1993@gmail.com>
2024-05-14 23:19:40 +0100

    added color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: "#f57722",
+      secondary: "#33475B",
+      tertiary: "#0A6ACB",
+      quaternary: "#67aded",
+      white: "#EBEBEB",
+      yellow: "#ff9d2a",
+    },
     extend: {},
   },
   plugins: [],
