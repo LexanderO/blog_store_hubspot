8906df3287700060485434be09c1d78b15304077
LexanderO <alexanderowens1993@gmail.com>
2024-05-14 23:21:39 +0100

    added welcome text and styles

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -4,12 +4,20 @@ const LandingPage = () => {
   };
 
   return (
-    <button
-      onClick={handleAuth}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-    >
-      Authorize with HubSpot
-    </button>
+    <div className="flex justify-center items-center">
+      <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+        <h1 className="text-8xl text-primary fort-bold">Hello <span className="text-3xl font-bold italic text-secondary">and</span></h1>
+        <h3 className="text-6xl  text-secondary font-bold">Welcome to <span className="text-primary">HubSpot</span> Blog Store</h3>
+        <div className="flex justify-center pt-6">
+        <button
+          onClick={handleAuth}
+          className="bg-primary hover:bg-quaternary text-white transition duration-150 ease-in-out font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Authorize with HubSpot
+        </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
