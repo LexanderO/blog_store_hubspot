837167dedba008a8d1798fe784dc28bd5009fe9a
LexanderO <alexanderowens1993@gmail.com>
2024-05-14 23:23:29 +0100

    fixed popup message and updated styles

--- frontend/src/components/MyBlogs.jsx
+++ frontend/src/components/MyBlogs.jsx
@@ -53,50 +53,52 @@ const MyBlogs = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">
-        My HubSpot Blogs
-      </h1>
-      {loading ? (
-        <p className="text-center text-blue-500">Loading...</p>
-      ) : (
-        blogs.map((blog) => (
-          <div
-            key={blog.id}
-            className="bg-white rounded-lg shadow-md p-6 mb-4 flex justify-between items-start"
-          >
-            <div>
-              <h3 className="text-xl font-semibold text-gray-800">
-                {blog.name}
-              </h3>
-              <p className="text-gray-600">{blog.meta_description}</p>
-              <p className="text-gray-500 italic pt-3">
-                Author: {blog.author_name || "Unknown"}
-              </p>
-              <a
-                href={blog.absolute_url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:text-blue-600 transition duration-300 ease-in-out"
-              >
-                Full Blog
-              </a>
-            </div>
-            <button
-              onClick={() => saveBlog(blog)}
-              className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    <>
+      <div className="max-w-4xl py-8">
+        <h1 className="text-3xl font-bold text-center text-primary mb-6">
+          My HubSpot Blogs
+        </h1>
+        {loading ? (
+          <p className="text-center text-blue-500">Loading...</p>
+        ) : (
+          blogs.map((blog) => (
+            <div
+              key={blog.id}
+              className="bg-white rounded-lg shadow-md p-6 mb-4 flex justify-between items-start"
             >
-              Upload
-            </button>
-          </div>
-        ))
-      )}
+              <div>
+                <h3 className="text-xl font-semibold text-gray-800">
+                  {blog.name}
+                </h3>
+                <p className="text-secondary font-medium">{blog.meta_description}</p>
+                <p className="text-gray-500 italic pt-3">
+                  Author: {blog.author_name || "Unknown"}
+                </p>
+                <a
+                  href={blog.absolute_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-tertiary hover:text-primary transition duration-300 ease-in-out"
+                >
+                  Full Blog
+                </a>
+              </div>
+              <button
+                onClick={() => saveBlog(blog)}
+                className="ml-4 bg-primary hover:bg-quaternary transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Upload
+              </button>
+            </div>
+          ))
+        )}
+      </div>
       {notification && (
-        <p className="bottom-0 fixed text-xl font-medium py-auto text-green-500">
+        <p className="top-24 py-10 bg-secondary/85  w-full fixed text-2xl font-medium text-white z-0 text-center">
           {notification}
         </p>
       )}
-    </div>
+    </>
   );
 };
 
